aba2c046495f41d7cd9d2e1547c6d77d41f135ad
unknown <tylerbroere@gmail.com>
2020-03-10 16:23:25 +0100

    modified back-button from core

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,9 +1,32 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
+import { isAndroid } from "tns-core-modules/platform/platform";
+import { Page } from "tns-core-modules/ui/page/page";
 
+declare var android: any;
 @Component({
     selector: "ns-current-challenge",
     templateUrl: "./current-challenge.component.html",
     styleUrls: ["./current-challenge.component.css"],
     moduleId: module.id
 })
-export class CurrentChallengeComponent {}
+export class CurrentChallengeComponent {
+    constructor(private router: RouterExtensions, private page: Page) {}
+
+    onTap() {
+        this.router.navigate(["/edit-challenge"]);
+    }
+
+    onLoadedActionBar() {
+        if (isAndroid) {
+            const androidToolbar = this.page.actionBar.nativeView;
+            const backButton = androidToolbar.getNavigationIcon();
+            if (backButton) {
+                backButton.setColorFilter(
+                    android.graphics.Color.parseColor("#171717"),
+                    (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+                );
+            }
+        }
+    }
+}
--- src/app/challenges/today/today.component.ts
+++ src/app/challenges/today/today.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
 
 @Component({
   selector: 'ns-today',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodayComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: RouterExtensions){}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(){
+
+    }
+
+    onTap(){
+        this.router.navigate(['/current-challenge']);
+    }
 
 }
