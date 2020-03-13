2e10b6aa811879df3b8bcc88a1bca889ad7e91b4
unknown <tylerbroere@gmail.com>
2020-03-13 10:34:39 +0100

    added tab content; hid double action bars

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,29 +1,33 @@
-import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component';
+import { ChallengeTabsComponent } from "./challenges/challenge-tabs/challenge-tabs.component";
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
 import { TodayComponent } from "./challenges/today/today.component";
-import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
-import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-edit.component';
+import { CurrentChallengeComponent } from "./challenges/current-challenge/current-challenge.component";
+import { ChallengeEditComponent } from "./challenges/challenge-edit/challenge-edit.component";
 
 const routes: Routes = [
     { path: "", component: AuthComponent },
-    {
-        path: "today",
-        component: TodayComponent
-    },
-    {
-        path: "current-challenge",
-        component: CurrentChallengeComponent
-    },
     {
         path: "edit-challenge",
         component: ChallengeEditComponent
     },
     {
         path: "challenges",
-        component: ChallengeTabsComponent
+        component: ChallengeTabsComponent,
+        children: [
+            {
+                path: "today",
+                component: TodayComponent,
+                outlet: "today"
+            },
+            {
+                path: "current-challenge",
+                component: CurrentChallengeComponent,
+                outlet: "currentChallenge"
+            }
+        ]
     }
 ];
 
